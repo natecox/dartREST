acf8377a79b5a316d6994d428b6e70e78f05e6d4
unknown <fcc-labs@WFCBE127-14.SCCCD.NET>
2014-02-22 13:24:42 -0800

    improved the create_request function

--- web/create_request.dart
+++ web/create_request.dart
@@ -1,12 +1,19 @@
 import 'dart:html';
+import 'dart:convert' show JSON;
 
 void create_request() {
     InputElement url_input = querySelector('#url');
     InputElement method_input = querySelector('#method');
-    ElementList data_elements = querySelector('.data');
+    ElementList data_elements = querySelectorAll('.data');
     
     Map data_map = new Map();
-    data_elements.forEach((element) => data_map[element.querySelector('.name')] = element.querySelector('.value'));
+    for (Element element in data_elements) {
+        InputElement name_input = element.querySelector('.name');
+        InputElement value_input = element.querySelector('.value');
+        if (name_input.value != '' && value_input.value != '') {
+            data_map[name_input.value] = value_input.value;
+        }
+    }
     
-    
+    String data_JSON = JSON.encode(data_map);
 }
