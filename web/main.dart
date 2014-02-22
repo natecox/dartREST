8a88ad5e2abc698a15f905a9738a662b94d8fea3
unknown <fcc-labs@WFCBE127-14.SCCCD.NET>
2014-02-22 15:27:49 -0800

    added missing style files

--- web/main.dart
+++ web/main.dart
@@ -4,10 +4,14 @@ import 'dart:html';
 import 'dart:convert' show JSON;
 
 part 'serverResponse.dart';
+part 'create_request.dart';
 
 void  main() {
 
     document.querySelector('.request-form').onSubmit.listen(create_request);
+    document.querySelectorAll('.method-selector').forEach((element) => element.onClick((event) {
+        (document.querySelector('.method') as InputElement).value = event.target.text;
+    }));
     //RESTRequest r = new RESTRequest();
     //Map map = new Map();
     //map['test'] = 'response';
