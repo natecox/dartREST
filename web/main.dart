7c4114ea565ad1d02028fcf2d7b41a6d1a2159ac
Nathan Cox <nathanc@bixly.com>
2014-02-22 14:32:41 -0800

    added server response

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,14 @@
+library dartrest;
+
 import 'dart:html';
-import 'dart:async' show Future;
+import 'dart:convert' show JSON;
+
+part 'serverResponse.dart';
 
 void  main() {
+    RESTRequest r = new RESTRequest();
+    Map map = new Map();
+    map['test'] = 'response';
+    map['another test'] = 'another response';
+    print(r.getMap('http://httpbin.org/post', 'POST', map).toString());
 }
