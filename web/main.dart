4b71f09aa2d8f54741ff79f30f86a96fb6ed79e2
unknown <fcc-labs@WFCBE127-14.SCCCD.NET>
2014-02-22 15:49:39 -0800

    work on index

--- web/main.dart
+++ web/main.dart
@@ -15,7 +15,7 @@ void  main() {
         document.querySelector('.dropdown-toggle').innerHtml = event.target.text + ' <span class="caret">';
         
     }));
-    document.querySelector('.parameter-title').onClick.listen(add_data_element);
+    document.querySelector('.add-parameter-btn').onClick.listen(add_data_element);
     //RESTRequest r = new RESTRequest();
     //Map map = new Map();
     //map['test'] = 'response';
